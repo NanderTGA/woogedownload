2f798c7f7e3070ecd6f4f1b2cf688dc4134b1c67
Perisan <124375533+PerisanCat@users.noreply.github.com>
2023-02-07 17:39:31 +0100

    H

--- assets/content.js
+++ assets/content.js
@@ -1,11 +1,12 @@
-    $(function() {
+$(function() {
 
-   $.getJSON('../content-data.json', function(data) {
-       $.each(data.content, function(i, f) {
-          var html = "<div class='content-box'><img src='" + f.image + "'><br><a href='./" + f.page-name + ".html'><h3>" + f.name + "</h3></a><br><span class='text-muted'>" + f.description + "</span></div>"
-           $(html).appendTo("#all-content");
-     });
-
-   });
-
-});
+    $.getJSON('../content-data.json', function(data) {
+        $.each(data.content, function(i, f) {
+           var html = `<div class="content-box"><img src="${f.image}"><br><a href="./${f.pagename}.html"><h3>${f.name}</h3></a><br><span class="text-muted">${f.description}</span></div>`
+            $(html).appendTo("#all-content");
+      });
+ 
+    });
+ 
+ });
+ 
