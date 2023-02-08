18d5f0d98c60f0a486e236109d65c46592b521fa
Perisan <124375533+PerisanCat@users.noreply.github.com>
2023-02-08 10:28:01 +0100

    Update content.js

--- assets/content.js
+++ assets/content.js
@@ -3,7 +3,7 @@ $(function() {
     $.getJSON('../content-data.json', function(data) {
         $.each(data.content, function(i, f) {
            var html = `<div id="content-box"><img src="${f.image}" class="thumb"><br><a href="./${f.pagename}.html"><h1>${f.name}</h1></a><span class="text-muted">${f.description}</span></div>`
-            $(html).appendTo("#all-content");
+            $(html).appendTo("#content-list");
       });
  
     });
